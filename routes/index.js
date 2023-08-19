2c9c1edf47c83928b68cafbb55d63182283abe1d
Tony W <bobacrypto2@gmail.com>
2023-08-18 23:02:50 -0700

    Messages now show up when we get index view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
+const messages = [
+  {
+    text: "Hello!",
+    user: "Tony",
+    added: new Date(),
+  },
+  {
+    text: "Hey there!",
+    user: "Katie",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
 module.exports = router;
